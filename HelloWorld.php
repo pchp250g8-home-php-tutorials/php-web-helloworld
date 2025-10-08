0819e3467cc5314e799df97da47a31383d59d284
pchp250g8-home <pchp250g8@tutamail.com>
2025-10-08 15:09:14 +0300

    Modified Project HelloWorld

--- HelloWorld.php
+++ HelloWorld.php
@@ -1,5 +1,7 @@
 <?php
+    $date = new DateTime("now");
     echo("Hello,World!!!<br>");
-    echo("PHP Test Web Page!!!");
-    phpinfo();
+    echo("PHP Test Web Page!!!<br>");
+    echo("Today is:". $date->format(DATE_RSS) ."!!!<br>");
+    echo("<a href=\"index.html\">Html Page</a>");
 ?>
